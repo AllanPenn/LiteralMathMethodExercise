bfa45f34a8da36ecb7386397392cdfd29db4ced3
Allan Pennycuff <allanp1238@gmail.com>
2021-09-23 10:18:33 -0500

    Corrected script.js and completed the bonus

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let warmHugs = "Hi, I\'m Olaf and I like warm hugs.";
 console.log(warmHugs.toUpperCase());
-console.log(warmHugs.replace("like", "love"));
+warmHugs = warmHugs.replace("like", "love")
+console.log(warmHugs);
 let beenImpaled = "Oh, look at that. I\'ve been impaled.";
-console.log(beenImpaled.indexOf("I've been impaled."));
+console.log(beenImpaled.slice(18));
 let dotDotDot = "...";
-let skullBones = "I don't have a skull" + dotDotDot + " or bones.";
+let skullBones = `I don't have a skull${dotDotDot}or bones.`;
 console.log(Math.PI);
 let randomNumber = Math.random();
 randomNumber *= 3;
 randomNumber = Math.floor(randomNumber);
 randomNumber++;
 console.log(randomNumber);
+
+// BONUS
+console.log(" Let It Go!".toUpperCase().repeat(2).trim());
+let reindeers = "Reindeers are better than people.";
+console.log(reindeers.replace(/ /g, "_"));
+console.log(Math.SQRT2);
+var newRandomNumber = Math.floor(Math.random() * (23 - 7 + 1) + 7)
