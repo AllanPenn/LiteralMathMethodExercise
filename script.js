0e2c237f7d8b3ece638ca6d2540070a1760e7eac
Allan Pennycuff <allanp1238@gmail.com>
2021-09-23 08:13:31 -0500

    Created script.js file for LiteralMathMethodExercise

---
+++ script.js
@@ -0,0 +1,13 @@
+let warmHugs = "Hi, I\'m Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+let beenImpaled = "Oh, look at that. I\'ve been impaled.";
+console.log(beenImpaled.indexOf("I've been impaled."));
+let dotDotDot = "...";
+let skullBones = "I don't have a skull" + dotDotDot + " or bones.";
+console.log(Math.PI);
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber = Math.floor(randomNumber);
+randomNumber++;
+console.log(randomNumber);
